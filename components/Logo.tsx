663722386577e162837d52398a0ff595580adcfb
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-25 14:12:36 +0900

    feat:full width logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import { tw } from "@twind";
 
 export default function Logo() {
   return (
-      <div class={tw`flex flex-row items-center justify-start w-1/3`}>
+      <div class={tw`flex flex-row items-center justify-start`}>
         <img src="/logo.png" class={tw`rounded w-10 h-10 m-4`} />
         <h1 class={tw`text-xl font-extrabold`}>
           Deno Korea
