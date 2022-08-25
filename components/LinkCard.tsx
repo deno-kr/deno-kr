aae4afcf384dbd629e59193c0a9e5444371b13d6
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-25 13:17:34 +0900

    feat:add some more description

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -11,7 +11,7 @@ interface LinkCardProps {
 
 export default function LinkCard({ icon, title, content, url }: LinkCardProps) {
   return (
-    <a href={url} class={tw`rounded bg-gray-200 w-full h-full p-4 m-4 grow flex flex-col transition-shadow hover:(shadow-xl)`}>
+    <a href={url} class={tw`rounded bg-gray-200 h-max p-4 flex flex-col transition-shadow hover:(shadow-xl)`}>
       <header class={tw`flex flex-row justify-start items-center`}>
         <img src={icon} alt="logo" class={tw`w-5 h-5`}/>
         <h2 class={tw`font-bold mx-2`}>{ title }</h2>
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,13 +2,21 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Logo from "../components/Logo.tsx";
-import Card from "../components/LinkCard.tsx";
+import LinkCard from "../components/LinkCard.tsx";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <Logo />
+      <a href="/">
+        <Logo />
+      </a>
       <p class={tw`m-4`}>
+        <a href="/" class={tw`text-blue-400`}>
+          Deno Korea
+        </a>
+        에 오신 것을 환영합니다.
+        <br />
+        <br />
         <a href="https://deno.land" class={tw`text-blue-400`}>
           Deno
         </a>
@@ -34,9 +42,10 @@ export default function Home() {
         </a>
         로 실행하는 런타임입니다.
       </p>
-      <div class={tw`flex flex-row w-full justify-evenly`}>
-        <Card icon="/icons/discord.svg" title="디스코드" content="Deno Korea 디스코드 참여하기" url="https://discord.com/invite/5HwyPHkaWq" />
-        <Card icon="/icons/github.svg" title="Github" content="Deno Korea Github Organization" url="https://github.com/deno-kr" />
+      <div class={tw`w-max grid grid-cols-2 grid-flow-row gap-4 m-4`}>
+        <LinkCard icon="/icons/deno_hr.svg" title="Deno" content="Deno 공식 홈페이지" url="https://deno.land" />
+        <LinkCard icon="/icons/discord.svg" title="디스코드" content="Deno Korea 디스코드 참여하기" url="https://discord.com/invite/5HwyPHkaWq" />
+        <LinkCard icon="/icons/github.svg" title="Github" content="Deno Korea Github Organization" url="https://github.com/deno-kr" />
       </div>
     </div>
   );
