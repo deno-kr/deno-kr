8c03d4e19965d5254f459d20de4951357857a624
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-25 11:26:29 +0900

    feat:add logo

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,9 +5,12 @@ import { tw } from "@twind";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <h1 class={tw`text-xl font-bold`}>
-        Deno Korea
-      </h1>
+      <div class={tw`flex flex-row items-center justify-start w-1/3`}>
+        <img src="/logo.png" class={tw`rounded w-10 h-10 m-4`} />
+        <h1 class={tw`text-xl font-bold`}>
+          Deno Korea
+        </h1>
+      </div>
     </div>
   );
 }
