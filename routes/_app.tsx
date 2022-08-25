92b16de3135e74b0f4a4ba59951837a6ed0af6f8
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-25 14:01:03 +0900

    feat:add footer

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,18 +1,25 @@
 /** @jsx h */
-import { h } from "preact";
+/** @jsxFrag Fragment */
+import { h, Fragment } from "preact";
 import { AppProps } from "$fresh/server.ts"
 import { tw } from "@twind";
 import { Head } from "$fresh/runtime.ts";
+import Footer from "../components/Footer.tsx";
 
 export default function App({ Component }: AppProps) {
   return (
-    <div class={tw`h-screen`}>
+    <>
       <Head>
         <title>Deno Korea</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles/global.css" />
       </Head>
-      <Component />
-    </div>
+      <div class={tw`flex flex-col min-h-screen`}>
+        <div class={tw`flex-1`}>
+          <Component />
+        </div>
+        <Footer />
+      </div>
+    </>
   )
 }
