2f959437199d6a9265d65b3e8b7263dbe5152522
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-25 11:12:36 +0900

    feat:dummy _app.tsx

---
+++ routes/_app.tsx
@@ -0,0 +1,17 @@
+/** @jsx h */
+import { h } from "preact";
+import { AppProps } from "$fresh/server.ts"
+import { tw } from "@twind";
+import { Head } from "$fresh/runtime.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <div class={tw`h-screen`}>
+      <Head>
+        <title>Deno Korea</title>
+        <link rel="stylesheet" href="/styles/global.css" />
+      </Head>
+      <Component />
+    </div>
+  )
+}
